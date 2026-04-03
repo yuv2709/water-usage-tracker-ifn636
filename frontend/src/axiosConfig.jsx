3ba812d8fa16f86951b6972cc52b4a9bf7ef26f4
Yuvraj <yakshitsachdeva277@gmail.com>
2026-04-03 20:29:06 +1000

    Connected remote backend with the frontend

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
+  baseURL: '/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
